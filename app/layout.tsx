db0f6113ae4593899ce5d75fe9b284f39e622dfb
0xOthmane <0xothodor@proton.me>
2024-02-07 16:55:40 +0100

    feat: add initial modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -16,15 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className} suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
         <ThemeProvider
           defaultTheme="system"
           attribute="class"
           enableSystem
           storageKey="discord-theme"
         >
-          {children}
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import { InitialModal } from '@/components/molecules/InitialModal';
 import { ModeToggle } from './../components/atoms/ModeToggle';
 
 export default function Home() {
   return (
     <div className="">
       <h1 className="text-3xl font-bold text-indigo-500">Discord clone</h1>
-
       <ModeToggle />
+      <InitialModal />
     </div>
   );
 }
