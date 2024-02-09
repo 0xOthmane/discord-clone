3a8a8817c6b89212b6d2f421be84cd58020c5226
0xOthmane <0xothodor@proton.me>
2024-02-09 11:37:30 +0100

    feat: add login route and form component

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from "@/components/molecules/SignInForm";
+
+const Login = () => {
+  return (
+    <div className="w-full">
+      <SignInForm />
+    </div>
+  );
+};
+
+export default Login;
