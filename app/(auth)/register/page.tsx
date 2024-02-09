7bd0f96e8057f2f490b2038bf2b9af6b8a429a17
0xOthmane <0xothodor@proton.me>
2024-02-09 11:37:13 +0100

    feat: add register route and form component

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import SignUpForm from "@/components/molecules/SignUpForm";
+
+const Register = () => {
+  return (
+    <div className="w-full">
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Register;
