b46ae2251830481af6d5e9ec55726980f6c1600f
0xOthmane <0xothodor@proton.me>
2024-02-11 13:40:45 +0100

    feat: add server sidebar

---
+++ app/(dashboard)/(routes)/servers/[serverId]/layout.tsx
@@ -0,0 +1,37 @@
+import { currentProfile } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+import db from "@/lib/db";
+import ServerSidebar from "@/components/organisms/ServerSidebar";
+
+const ServerIdLayout = async ({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: { serverId: string };
+}) => {
+  const profile = await currentProfile();
+  if (!profile) return redirect("/login");
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+  if (!server) return redirect("/");
+  return (
+    <div className="h-full">
+      <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+        <ServerSidebar serverId={params.serverId}/>
+      </div>
+      <main className="h-full md:pl-60">{children}</main>
+    </div>
+  );
+};
+
+export default ServerIdLayout;
