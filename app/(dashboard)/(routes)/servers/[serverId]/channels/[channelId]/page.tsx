1bc660ed5f72c9d5d1770a39a52c7ddf14583398
0xOthmane <0xothodor@proton.me>
2024-02-14 19:36:09 +0100

    feat: add channel id page

---
+++ app/(dashboard)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -0,0 +1,6 @@
+const ChannelIdPage = () => {
+    return <div>Channel id</div>;
+  };
+  
+  export default ChannelIdPage;
+  
--- app/(dashboard)/(routes)/servers/[serverId]/page.tsx
+++ app/(dashboard)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,37 @@
-const ServerIdPage = ()=>{
-return (
-    <div>Server Id page</div>
-)
-}
+import { currentProfile } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
-export default ServerIdPage
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+  if (!profile) return redirect("/login");
+  const { serverId } = params;
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: { createdAt: "asc" },
+      },
+    },
+  });
+  const initialChannel = server?.channels[0];
+  if (initialChannel?.name !== "general") return null;
+  return redirect(`/servers/${serverId}/channels/${initialChannel?.id}`);
+};
+
+export default ServerIdPage;
